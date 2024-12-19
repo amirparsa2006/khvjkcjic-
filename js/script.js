c304e155bcc4d2cde68bc1e57a485e5e3f3225e9
parsa <parsasalmanipour@gmail.com>
2024-12-18 23:36:53 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// const checkbox = document.querySelector("#day-night");
+// const body = document.querySelector("body");
+// const dayClass = 'day';
+
+// checkbox.addEventListener('change', (e) => {
+//   const { checked } = e.target;
+//   if (checked) {
+//     body.classList.add(dayClass);
+//   } else {
+//     body.classList.remove(dayClass);
+//   }
+// });
+// جبر
+// 2 + 2 = 4
+// a = 204
+// b = 286
+// c = a+ b
+//نحوه تعریف متغیر
+var a = 2;
+a = 12;
+a = 85; //reasign
+var a = 96; // redifind تعریف دوباره
+let c = 32;
+c = 45; //reasign 
+c = 20;
+// let c = 90;  این خطا میدهد
+const d = a + c;
+// d = 12; غلط است
+// const d = 53; غلط 
+
+//انواع متغییر 
+mi = 12;
+mi = "hello";
+mi = 'hello';
+mi = `hello`;
+mi = 12.63;
+mi = [12,13,14];
+mi = [[12,13],[14,15],[16,17]];
+mi = [12,13,"abc",12.3]
+mi = null;
+mi = true;
+mi = false;
+mi = {
+
+};
+car = {
+  color     : "white",
+  tire      : 4,
+  windshild : true,
+
+}
+function  milad(){
+
+}
+
+function sum(){
+  a + b 
+}
+console.log("hello");
+a = 12;
+console.log(a);
+console.log("a");
